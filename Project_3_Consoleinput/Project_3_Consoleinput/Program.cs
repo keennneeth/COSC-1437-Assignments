c6ee8437e8d36c36b1fd676e9f4a69278adde045
Kenneth Rodriguez <64386216+keennneeth@users.noreply.github.com>
2020-09-21 22:48:59 -0500

    First Name, Last Name Project

---
+++ Project_3_Consoleinput/Project_3_Consoleinput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Project_3_Consoleinput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = "Kenneth";
+            string lastname = "Rodriguez";
+
+            Console.WriteLine("Name: " + firstname + " " + lastname);
+
+            Console.WriteLine("Please enter a new first name: ");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("Please enter a new last name: ");
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("New Name: " + firstname + " " + lastname);
+
+            Console.Write("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
