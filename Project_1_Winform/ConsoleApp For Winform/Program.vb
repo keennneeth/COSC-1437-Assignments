b86c922d7283bc739b37a54c4396bd29e6063c66
Kenneth Rodriguez <64386216+keennneeth@users.noreply.github.com>
2020-09-21 22:34:35 -0500

    Hello World Console
    Hello world console completed

---
+++ Project_1_Winform/ConsoleApp For Winform/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Project 2")
+        Console.WriteLine("By Kenneth Rodriguez")
+        Console.WriteLine()
+
+        Console.Write("Press Any Key To Continue")
+        Console.ReadKey()
+    End Sub
+End Module
