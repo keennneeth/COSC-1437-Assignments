26999eeaca1675026bf764b83f152d340f55d764
Kenneth Rodriguez <64386216+keennneeth@users.noreply.github.com>
2020-09-29 14:19:42 -0500

    Part 1 complete

--- Weekly Topic Unit 2/CustomerAndInventory_Tests/Order_Tests.cs
+++ Weekly Topic Unit 2/CustomerAndInventory_Tests/Order_Tests.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CustomerAndInventory_Tests
 {
-    class Order_Tests
+    [TestClass]
+    public class Order_Tests
     {
+        [TestMethod]
+        public void Verify_The_ID_Is_Zero_When_Instantiated()
+        {
+            //assign
+            var customer = new CustomerAndInventory.Customer();
+
+            //action
+
+            //assert
+            Assert.AreEqual(expected: 0, actual: customer.ID);
+        }
+        [TestMethod]
+        public void Verify_The_ID_Can_Be_Assigned()
+        {
+            //assign
+            var assignedID = 1234;
+            var customer = new CustomerAndInventory.Customer();
+            //action
+            customer.ID = assignedID;
+
+            //assert
+            Assert.AreEqual(expected: assignedID, actual: customer.ID);
+
+        }
     }
 }
