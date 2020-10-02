5e444c809a55250eeb8fa7b31906f59725ac4463
Kenneth Rodriguez <64386216+keennneeth@users.noreply.github.com>
2020-10-02 11:33:47 -0500

    Topic Unit 2 Fixed
    Changed unit test format from .NET Framework to .NET Core

---
+++ Weekly Topic Unit 2/CustomerAndInventory_Tests_Core/Order_Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*
+* Kenneth Rodriguez
+*/
+
+namespace CustomerAndInventory_Tests
+{
+    [TestClass]
+    public class Order_Tests
+    {
+        [TestMethod]
+        public void Verify_The_ID_Is_Zero_When_Instantiated()
+        {
+            //assign
+            var customer = new CustomerAndInventory.Customer();
+
+            //action
+
+            //assert
+            Assert.AreEqual(expected: 0, actual: customer.ID);
+        }
+        [TestMethod]
+        public void Verify_The_ID_Can_Be_Assigned()
+        {
+            //assign
+            var assignedID = 1234;
+            var customer = new CustomerAndInventory.Customer();
+            //action
+            customer.ID = assignedID;
+
+            //assert
+            Assert.AreEqual(expected: assignedID, actual: customer.ID);
+        }
+        [TestMethod]
+        public void Check_Proper_Assignment_Of_The_Customer_ID()
+        {
+            //assign
+            var customer = new CustomerAndInventory.Customer();
+
+            //action
+            customer.ID = 1234;
+
+            //assert
+            Assert.AreEqual(1234, customer.ID);
+        }
+
+    }
+}
+
+
