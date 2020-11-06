476108ef64f64f57dcb5ccea8ef9c6ca6619158b
Kenneth Rodriguez <64386216+keennneeth@users.noreply.github.com>
2020-11-06 14:15:36 -0600

    encryption complete

--- Mastery Solution Unit 7/Main Application/MainForm.Designer.cs
+++ Mastery Solution Unit 7/Main Application/MainForm.Designer.cs
@@ -34,13 +34,13 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReadEncryptedFile = new System.Windows.Forms.Button();
+            this.btnWriteEncryptedFile = new System.Windows.Forms.Button();
             this.btnOpenFile = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.IbFileOutput = new System.Windows.Forms.ListBox();
             this.txtFileName = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.statusStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -56,7 +56,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Exit Application";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // pictureBox1
             // 
@@ -86,14 +86,34 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnReadEncryptedFile);
+            this.panel1.Controls.Add(this.btnWriteEncryptedFile);
             this.panel1.Controls.Add(this.btnOpenFile);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(175, 335);
             this.panel1.TabIndex = 4;
+            // 
+            // btnReadEncryptedFile
+            // 
+            this.btnReadEncryptedFile.Location = new System.Drawing.Point(32, 184);
+            this.btnReadEncryptedFile.Name = "btnReadEncryptedFile";
+            this.btnReadEncryptedFile.Size = new System.Drawing.Size(111, 47);
+            this.btnReadEncryptedFile.TabIndex = 1;
+            this.btnReadEncryptedFile.Text = "Read Encrypted File";
+            this.btnReadEncryptedFile.UseVisualStyleBackColor = true;
+            this.btnReadEncryptedFile.Click += new System.EventHandler(this.btnReadEncryptedFile_Click);
+            // 
+            // btnWriteEncryptedFile
+            // 
+            this.btnWriteEncryptedFile.Location = new System.Drawing.Point(32, 119);
+            this.btnWriteEncryptedFile.Name = "btnWriteEncryptedFile";
+            this.btnWriteEncryptedFile.Size = new System.Drawing.Size(111, 47);
+            this.btnWriteEncryptedFile.TabIndex = 1;
+            this.btnWriteEncryptedFile.Text = "Write Encrypted File";
+            this.btnWriteEncryptedFile.UseVisualStyleBackColor = true;
+            this.btnWriteEncryptedFile.Click += new System.EventHandler(this.btnWriteEncryptedFile_Click);
             // 
             // btnOpenFile
             // 
@@ -143,27 +163,6 @@
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(299, 335);
             this.panel3.TabIndex = 6;
-            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(32, 119);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(111, 47);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Write Encrypted File";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.btnWriteEncryptedFile);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(32, 184);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(111, 47);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Read Encrypted File";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.btnReadEncryptedFile);
             // 
             // MainForm
             // 
@@ -206,8 +205,8 @@
         private System.Windows.Forms.Button btnOpenFile;
         private System.Windows.Forms.ListBox IbFileOutput;
         private System.Windows.Forms.TextBox txtFileName;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnReadEncryptedFile;
+        private System.Windows.Forms.Button btnWriteEncryptedFile;
     }
 }
 
---
+++ Mastery Solution Unit 7/Main Application/MainForm.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoreLibrary;
+using CoreLibrary.Extensions;
+
+namespace Master_Project
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = Properties.Settings.Default.PreviousFilePath;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+
+        {
+            IbFileOutput.Items.Clear();
+
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = @"Comma seperated values |*.csv|Text File| *.txt",
+                Title = @"Select the Hundred Names database"
+            };
+
+            // Shows Dialog
+            // When user clicks ok, it opens
+
+            var dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            { 
+            txtFileName.Text = openFileDialog.SafeFileName;
+            toolStripStatusLabel1.Text = openFileDialog.FileName;
+
+
+
+            StreamReader sr = File.OpenText(openFileDialog.FileName);
+            string sourceFileContent = sr.ReadToEnd();
+
+            var parsedArrayOfStrings = Regex.Split(sourceFileContent, "\n");
+            IbFileOutput.Items.AddRange(parsedArrayOfStrings);
+
+        }
+
+        }
+
+        private void btnWriteEncryptedFile_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = @"Comma Seperated Values |*.csv| Text File| *.txt| All Files| *.*",
+                Title = @"Select a file, preferably a text file"
+            };
+
+            var dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+
+            {
+                txtFileName.Text = openFileDialog.SafeFileName;
+                toolStripStatusLabel1.Text = openFileDialog.FileName;
+
+                // reads the plain text
+
+                string inputFilePath = openFileDialog.FileName;
+                FileStream sourceFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read);
+
+                Debug.WriteLine($"Property 'Name' : {sourceFileStream.Name}");
+                Debug.WriteLine($"Property 'CanRead' : {sourceFileStream.CanRead}");
+
+                var sr = new StreamReader(sourceFileStream);
+                Debug.WriteLine($"Property 'EndOfStream' : {sr.EndOfStream}");
+                var sourceFileContent = sr.ReadToEnd();
+
+                Debug.WriteLine($"Property 'EndOfStream' : {sr.EndOfStream}");
+                Debug.WriteLine(sourceFileContent.Left(25));
+
+
+                // Writes the encrypted File
+
+                var outputFilePath = Path.ChangeExtension(openFileDialog.FileName, "Encrypted");
+                Debug.WriteLine($"outputFilePath : {outputFilePath}");
+                WriteEncrypt(outputFilePath, sourceFileContent);
+
+            }
+        }
+
+        private void btnReadEncryptedFile_Click(object sender, EventArgs e)
+        {
+            IbFileOutput.Items.Clear();
+
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = @"Comma Seperated Values |*.Encrypted",
+                Title = @"Select an encrypted File"
+            };
+
+            // shows the dialog
+
+            var dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+
+            {
+                txtFileName.Text = openFileDialog.SafeFileName;
+                toolStripStatusLabel1.Text = openFileDialog.FileName;
+                var fileContent = ReadEncrypt(openFileDialog.FileName);
+                var parsedArrayOfStrings = Regex.Split(fileContent, "\n");
+
+                IbFileOutput.Items.AddRange(parsedArrayOfStrings);
+            }
+        }
+
+        /// <summary>
+        /// Encrypt FileStream
+        /// </summary>
+        /// <param name="outputFilePath">full path to the output file</param>
+        /// <param name="msg">text to output</param>
+        private static void WriteEncrypt(string outputFilePath, string msg)
+        {
+            var outputFileStream =
+                new FileStream(outputFilePath,
+                    FileMode.Create,
+                    FileAccess.Write);
+
+            Debug.WriteLine($"Property 'Name' : {outputFileStream.Name}");
+            Debug.WriteLine($"Property 'CanRead' : {outputFileStream.CanRead}");
+
+            // (1) Create Data Encryption Standard (DES) object.
+            var crypt = new DESCryptoServiceProvider()
+            {
+                KeySize = 64,
+                Key = new byte[] { 61, 62, 63, 64, 65, 66, 67, 68 },
+                IV = new byte[] { 61, 62, 63, 64, 65, 66, 67, 68 }
+            };
+
+            // (2) Create a key and Initialization Vector - requires 8 bytes
+            // old key crypt.Key = new byte[] { 71, 72, 83, 84, 85, 96, 97, 78 };
+            // old key crypt.IV = new byte[] { 71, 72, 83, 84, 85, 96, 97, 78 };
+
+            // (3) Create CryptoStream stream object
+            CryptoStream cs =
+                new CryptoStream(outputFileStream,
+                    crypt.CreateEncryptor(),
+                    CryptoStreamMode.Write);
+            // (4) Create StreamWriter using CryptoStream
+            StreamWriter sw = new StreamWriter(cs);
+
+            sw.Write(msg);
+            sw.Close();
+            cs.Close();
+        }
+
+        /// <summary>
+        /// Read and Decrypt a file stream.
+        /// </summary>
+        /// <param name="inputFilePath">input file path name</param>
+        /// <returns></returns>
+        private static string ReadEncrypt(string inputFilePath)
+        {
+            var inputFileStream =
+                new FileStream(inputFilePath,
+                    FileMode.Open,
+                    FileAccess.Read);
+
+            Debug.WriteLine($"Property 'Name' : {inputFileStream.Name}");
+            Debug.WriteLine($"Property 'CanRead' : {inputFileStream.CanRead}");
+
+            // (1) Create Data Encryption Standard (DES) object.
+            var crypt = new DESCryptoServiceProvider()
+            {
+                KeySize = 64,
+                Key = new byte[] { 61, 62, 63, 64, 65, 66, 67, 68 },
+                IV = new byte[] { 61, 62, 63, 64, 65, 66, 67, 68 }
+            };
+
+            // (2) Create a key and Initialization Vector
+            // old crypt.Key = new byte[] { 71, 72, 83, 84, 85, 96, 97, 78 };
+            // old crypt.IV = new byte[] { 71, 72, 83, 84, 85, 96, 97, 78 };
+
+            // (3) Create CryptoStream stream object
+            var cs =
+                new CryptoStream(inputFileStream,
+                    crypt.CreateDecryptor(),
+                    CryptoStreamMode.Read);
+
+            // (4) Create StreamReader using CryptoStream
+            var sr = new StreamReader(cs);
+            var msg = sr.ReadToEnd();
+            sr.Close();
+            cs.Close();
+            return msg;
+        }
+    }
+
+}
+
